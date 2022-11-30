ea143a32dc0d6e0f7c4ab9319bfa67db030b6673
epikoder <efedua.bell@gmail.com>
2022-11-30 19:42:58 +0100

    fix

--- components/exam/questions.selection.component.tsx
+++ components/exam/questions.selection.component.tsx
@@ -10,12 +10,12 @@ interface ExamState {
 const QuestionSelection: FC<ExamState & { onClick?: (index: number) => void }> = (props) => {
     const { questions, onClick, current, current_index } = props
     return <div className="flex flex-wrap">
-        {questions?.map((q, i) => <div
+        {/* {questions?.map((q, i) => <div
             key={i}
             className={`px-2 py-1 m-1 cursor-pointer hover:opacity-80 duration-300 transition-all ease-in-out rounded text-white ${questions![i].answer !== '' ? 'bg-green-500' : 'bg-gray-400'}`}
             onClick={() => onClick !== undefined ? onClick(i) : {}}>
             {i + 1}
-        </div>)}
+        </div>)} */}
     </div>
 }
 
